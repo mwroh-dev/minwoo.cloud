43dadc81dec45c988172a349dbe95ed6977629e0
minwoo.roh <mwroh.dev@gmail.com>
2025-01-28 14:43:58 +0900

    feature: initialize main page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,30 @@
-import Image from 'next/image';
-
 export default function Home() {
   return (
-  <section
-    className="
-      w-full min-h-[calc(100vh-72px)]
-      flex items-center justify-center
-    ">
-      <Image
-        className="dark:invert"
-        src="/next.svg"
-        alt="Next.js logo"
-        width={180}
-        height={38}
-        priority
-      />
+    <section className="w-full min-h-[calc(100vh-72px)] flex flex-col items-end justify-center">
+      <div className="w-full h-[345px]">
+        <video
+          autoPlay
+          muted
+          loop
+          playsInline
+          className="w-full h-full object-cover rounded-xl"
+        >
+          <source src="/cielo.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+      <div className="mt-7">
+          <p className="
+            text-right text-xl font-bold text-gray-800
+            font-serif tracking-wider leading-relaxed">
+            Learning by Doing
+          </p>
+          <p className="
+            text-right text-xl font-bold text-gray-800
+            font-serif tracking-wider leading-relaxed">
+            Solving Problems with Code
+          </p>
+        </div>
     </section>
   );
 }
